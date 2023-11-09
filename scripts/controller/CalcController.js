40660ceec0db1ce1cdb4c49285c6af0cbd0bcaab
Mateus Mattos <mateusmattos6@gmail.com>
2023-11-09 18:54:35 -0300

    update

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,53 @@
+class CalcController {
+  constructor() {
+    this._locale = "pt-BR";
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this.currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    let interval = setInterval(() => {
+      this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+      this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }, 1000);
+  }
+
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    this._timeEl.innerHTML = value;
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    this._dateEl.innerHTML = value;
+  }
+
+  get displayCalc() {
+    return this._displayCalcEl.innerHTML;
+  }
+
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
+  }
+
+  get currentDate() {
+    return new Date();
+  }
+
+  set currentDate(value) {
+    this._currentDate = value;
+  }
+}
